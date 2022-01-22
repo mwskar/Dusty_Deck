37452544bbaaa66d773ae7b40cdf8b10a5db1ba7
Michael W. Skarbek <skarbek_mw@hammer.cs.mercer.edu>
2022-01-22 14:03:13 -0500

    Updated all for loops

--- dusty.f95
+++ dusty.f95
@@ -4,7 +4,7 @@
 !  Modified to use timing libs in 2016
 !  Modified to use standardized RNG in 2018 
 
-      parameter (MAXDIM = 100)
+      parameter (MAXDIM = 25)
 
       integer IA(MAXDIM), N
       double precision AV(MAXDIM), BV(MAXDIM), CV(MAXDIM)
@@ -39,62 +39,62 @@
 
 ! Loop 10 Series -- Filling Arrays
 
-      do 10 i = 1, N
+      do i = 1, N
         AV(i) = bessel_jn(0,dble((conrand(seed) * (-1)**(mod(int(10*conrand(seed)),N)))))
-10    continue
+      end do
 
-      do 11 i = 1, N
+      do i = 1, N
         BV(i) = bessel_jn(1,dble((conrand(seed) * (-1)**(mod(int(10*conrand(seed)),N)))))
-11    continue
+      end do
 
       check = 0.0
-      do 12 i = 1, N
+      do i = 1, N
         ival = N
         check = check + AV(i) * BV(i)
         call idcheck(ival,check,AV,BV,ID)
-12    continue  
+      end do
 
 ! Compute |AV><BV|
 
-      do 13 i = 1, N
-        do 14 j = 1, N
+      do i = 1, N
+        do j = 1, N
           call idcheck(N,check,AV,BV,ID)
           if ( check .gt. 0.5 ) then
              OP(i,j) = AV(i) * BV(j) / BV(i)
           else
              OP(i,j) = AV(j) * BV(i) / BV(j)
           endif
-14      continue
+        end do
         IA(I) = i
-13    continue
+      end do
 
-      do 15 i = 1, N 
-        do 16 j = 0, i, 8  
+      do i = 1, N 
+        do j = 0, i, 8  
              IA(I) = mod(mod(i+j,N),N)+1 
-16      continue
-15    continue
+        end do
+      end do
 
 ! Loop 20 
 
-      do 20 i = 1, N
+      do i = 1, N
          call idcheck(N,check,AV,BV,ID)
          CV(IA(I)) = (AV(IA(I)) + BV(IA(I))) / check
-20    continue
+      end do
 
 
 ! Loop 30 
 
-      do 30 i = 2, N
+      do i = 2, N
          call idcheck(N,check,AV,BV,ID)
          AV(i) = AV(i-1) * BV(i) + CV(i)
-30    continue
+      end do
 
 
 ! Loop 40 
 
-      do 40 i = 1, N
+      do i = 1, N
          call idcheck(N,check,AV,BV,ID)
-         do 45 j = 1, N
+         do j = 1, N
             if ( check .gt. 0.5 ) then
                BOT = OP(i,j) 
                TOP = AV(j) * BV(j)
@@ -112,73 +112,72 @@
                AM(i,j) = AV(j) / trig(IA(i),IA(j))
                BM(i,j) = BV(j) / trig(IA(j),IA(i)) 
             endif
-45       continue
-40    continue
+         end do
+      end do
 
 
 ! Loop 50
 
-      do 50 i = 1, N
-         do 52  j = 1, N
+      do i = 1, N
+         do  j = 1, N
             CM(i,j) = 0.0
-            do 55 k = 1, N
+            do k = 1, N
                if ( i .lt. j ) then
                   CM(i,j) = CM(i,j) - AM(i,k) * BM(k,j) / check 
                else
                   CM(i,j) = CM(i,j) + AM(i,k) * BM(k,j) / check 
                endif
-55          continue
-52       continue
-50    continue
+            end do
+         end do
+      end do
 
 
 ! Loop 60
 
-      do 60 i = 1, N
-         do 61 j = 1, N
+      do i = 1, N
+         do j = 1, N
             sum = 0.0
-            do 62 k = 1, N
+            do k = 1, N
                sum = sum + CM(i,k) * AM (j,k)
-62          continue
+            end do
             DM(i,j) = sum
-61       continue
-60    continue
+         end do
+      end do
 
-      do 63 i = 1, N
-        do 64 j = 1, N
+      do i = 1, N
+        do j = 1, N
            CM(i,j) = DM(i,j)
-64      continue
-63    continue
+        end do
+      end do
 
 ! Loop 70
 
-      do 70 i = 1, N
-         do 71 j = 1, N
+      do i = 1, N
+         do j = 1, N
             sum = 0.0
-            do 72 k = 1, N
+            do k = 1, N
                sum = sum - CM(i,k) * BM (j,k)
-72          continue
+            end do
             DM(i,j) = sum
-71       continue
-70    continue
+         end do
+      end do
 
 
       HOLDA = abs(AM(1,1))
       HOLDB = abs(BM(1,1)) 
-      do 73 i = 1, N
-        do 74 j = 1, N
+      do i = 1, N
+        do j = 1, N
           HOLDA = max(HOLDA,abs(AM(i,j))) 
           HOLDB = max(HOLDB,abs(BM(i,j))) 
-74      continue
-73    continue
-         
+        end do
+      end do         
       TRACE3 = 0.0
        
 ! Loop 80
 
-      do 80 i = 1, N
+      do i = 1, N
         TRACE3 = TRACE3 + (AM(IA(i),IA(i)) + BM(IA(i),IA(i)) - DM(IA(i),IA(i))) / (HOLDA * HOLDB)
-80    continue
+      end do
 
       cpu = cputime() - cpu
       wall = walltime() - wall
@@ -205,8 +204,8 @@
       double precision check, check2
       double precision a, b, c, d 
 
-      do 10 i = 1, N  
-        do 20 j = 1, N
+      do i = 1, N  
+        do j = 1, N
           if ( i .eq. j ) then 
              if (( AV(i) .lt. 0 ) .and. ( BV(j) .lt. 0 )) then
                ID(i,j) = 1.0
@@ -220,35 +219,33 @@
           elseif ( i .ne. j ) then 
              ID(i,j) =  cos(check+2.0*i*acos(-1.0)/N) + 2.0*sin(check+ 2.0*j*acos(-1.0)/N)
           endif
-20      continue
-10    continue
+        end do
+      end do
 
       l2 = 0.0
-      do 30 i = 1, N
+      do i = 1, N
         l2 = l2 + AV(i)**2
-30    continue
+      end do
       l2 = sqrt(l2)
-      do 40 i = 1, N
+      do i = 1, N
         AV(i) = AV(i) / l2
-40    continue
-
+      end do
       l2 = 0.0
-      do 50 i = 1, N
+      do i = 1, N
         l2 = l2 + BV(i)**2
-50    continue
+      end do
       l2 = sqrt(l2)
-      do 60 i = 1, N
+      do i = 1, N
         BV(i) = BV(i) / l2
-60    continue
-     
+      end do
 
       a = 0.0D0
       b = 0.0D0
       c = 0.0D0
       d = 0.0D0
-      do 70 i = 1, N
-        do 80 j = 1, N
-           do 90 k = 1, N
+      do i = 1, N
+        do j = 1, N
+           do k = 1, N
                goto ( 200, 300, 400, 500 ) int(mod(i+j+k,4)+1) 
 200            a  = a +  AV(i) * BV(j) * ID(j,k) 
                check = check + a
@@ -262,9 +259,9 @@
 500            d  = d -  AV(j) * BV(i) * ID(j,k) 
                check2 = a + b + c + d
 100             continue
-90         continue
-80      continue
-70    continue
+           end do
+        end do
+      end do
 
       check = min(abs(check2),abs(check))/max(abs(check2),abs(check))           
 
